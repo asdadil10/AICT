44584f8f0c0598fdd08f40abb288c9cf326062f9
asdadil10 <asdadil1024@gmail.com>
2025-12-12 10:33:54 +0500

    Visual tweaks

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let pausebtn=document.getElementById("centerbtn");
 pausebtn.onclick=pauseplay;
 function pauseplay()
 {
-    if(pausebtn.textContent=="II") //pause logic goes here
+    if(pausebtn.textContent=="I I") //pause logic goes here
         {
         pausebtn.textContent="▷";
         }
     else //play logic goes here
     {
-        pausebtn.textContent="II";
+        pausebtn.textContent="I I";
     }
 }
