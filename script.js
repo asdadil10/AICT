0e098065bbc0bcd5d117a9ec1fafcbadbd3e42c1
asdadil10 <asdadil1024@gmail.com>
2025-12-13 13:15:13 +0500

    added online tracks by ncs

--- script.js
+++ script.js
@@ -7,19 +7,61 @@ let currentTimeEl = document.getElementById("currentTime");
 let totalDurationEl = document.getElementById("totalDuration");
 let nextbtn = document.getElementById("rightbtn");
 let previousbtn = document.getElementById("leftbtn");
-const tracks = [
-    "happytrack1.mp3", "happytrack2.mp3", "happytrack3.mp3",
-    "sadtrack1.mp3", "sadtrack2.mp3", "sadtrack3.mp3",
-    "energetictrack1.mp3", "energetictrack2.mp3", "energetictrack3.mp3",
-    "motivatedtrack1.mp3", "motivatedtrack2.mp3", "motivatedtrack3.mp3"
-];
+let creditsbtn = document.getElementById("creditsbtn");
+
 let happy = document.getElementById("happycard");
 let sad = document.getElementById("sadcard");
 let energetic = document.getElementById("energeticcard");
 let motivated = document.getElementById("motivatedcard");
 let trackname;
+let creditsinfo;
 let vibe = 0; //0=happy,1=sad,2=energetic,3=motivated
+const tracks = [
+    "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/002/018/never-give-up-on-loving-you-1764896468-Csbo2JmIso.mp3", "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/758/on-and-on-1727308858-tLh4ktBdt5.mp3", "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/750/heroes-tonight-x-dreams-pt-ii-mashup-1725969652-gPmvlvBBTP.mp3",
+    "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/002/017/scars-1764896463-xsRdrCuldh.mp3", "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/002/012/fireflies-1764032464-3c4tlYQgRQ.mp3", "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/969/round-around-1758585663-KzABbY3nfl.mp3",
+    "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/002/022/lost-pt-ii-1765501267-X9EsqWRb8U.mp3", "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/002/021/see-the-sun-1765414865-S9PH5x4mCY.mp3", "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/002/019/needed-you-1765242065-eBG1mS8tyL.mp3",
+    "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/959/all-weve-ever-known-ft-fred-v-immy-odon-1756476063-giqRRYJNfs.mp3", "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/974/1759489943_WNu03JmYAO_LANCELOT-ACIGODE-SXYGX---Loop-NCS-Release.mp3", "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/002/004/mortals-rameses-b-remixs-1762477276-EqCZQdL0px.mp3"
+];
+const tracksJson = [
+    { "src": tracks[0], "name": "Never Give Up On Loving You","credits":"Song: KUČKA - Never Give Up On Loving You\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/NGUOLY\nWatch: ncs.lnk.to/NGUOLYAT/youtube" },
+    { "src": tracks[1], "name": "On and On","credits":"Song: BAYZY, Sayfro - On and On [NCS Release]\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/BS\\_OnandOn\nWatch: ncs.lnk.to/BS\\_OnandOnAT/youtube" },
+    { "src": tracks[2], "name": "Heroes Tonight x Dreams pt.II Mashup","credits":"Song: Heroes Tonight x Dreams pt. II Mashup\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/HTD" },
+    
+    { "src": tracks[3], "name": "scars","credits":"Song: lofin - scars\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/scars\nWatch: ncs.lnk.to/scarsAT/youtube" },
+    { "src": tracks[4], "name": "Fireflies","credits":"Song: Krezus & surreal_dvd - Fireflies\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/fireflies\nWatch: ncs.lnk.to/firefliesAT/youtube" },
+    { "src": tracks[5], "name": "Round Around","credits":"Song: HXI, Nateki - Round Around\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/roundaround\nWatch: ncs.lnk.to/roundaroundAT/youtube" },
+    
+    { "src": tracks[6], "name": "Lost pt.II","credits":"Song: Lost Sky x Shiah Maisel - Lost pt. II\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/lostptii\nWatch: ncs.lnk.to/lostptiiAT/youtube" },
+    { "src": tracks[7], "name": "See The Sun","credits":"Song: Abstrakt, weloveyouspydee - See The Sun\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/SEETHESUN\nWatch: ncs.lnk.to/SEETHESUNAT/youtube" },
+    { "src": tracks[8], "name": "Needed You by NOON","credits":"Song: NOON - Needed You\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/neededyou\nWatch: ncs.lnk.to/neededyouAT/youtube" },
+    
+    { "src": tracks[9], "name": "All We've Ever Known (ft. Fred V & Immy Odon)","credits":"Song: Cartoon - All We've Ever Known (ft. Fred V & Immy Odon)\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/allweveeverknown\nWatch: ncs.lnk.to/allweveeverknownAT/youtube" },
+    { "src": tracks[10], "name": "LOOP","credits":"Song: LANCELOT, ACIGODE, SXYGX - Loop\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/loop\nWatch: ncs.lnk.to/loopAT/youtube" },
+    { "src": tracks[11], "name": "Mortals (Rameses B Remix)","credits":"Song: Warriyo - Mortals (Rameses B Remix)\nMusic provided by NoCopyrightSounds\nFree Download/Stream: ncs.io/mortalsramesesbremix\nWatch: ncs.lnk.to/mortalsramesesbremixAT/youtube" },
 
+];
+function gettrackname()
+{
+    let currentSrc=audio.src;
+    const newTrack = tracksJson.find(function(track) {return currentSrc.includes(track.src);});  //put found object into newTrack
+    if (newTrack) //if newTrack is not empty
+        {
+        trackname= newTrack.name;
+        creditsinfo=newTrack.credits;
+        }
+         else //if empty
+        {
+        creditsinfo="Unknown Track, No credits."
+            trackname= "Unknown Track"; // If no match is found.
+        }
+    document.getElementById("trackname").querySelector('p').textContent = trackname;
+   
+}
+
+creditsbtn.onclick=function()
+{
+alert(creditsinfo);
+}
 function playaudio()
 {
     audio.play();
@@ -55,7 +97,7 @@ function playnexttrack()
         i=9;
         j=11;
     }
-    let currentindex = tracks.indexOf(trackname);
+    let currentindex = tracks.indexOf(audio.src);
     if (currentindex==j)
         currentindex=i;
     else
@@ -99,11 +141,7 @@ document.addEventListener('keydown', function(event) {
 //Setup Audio Duration when Metadata is loaded
 audio.onloadedmetadata = function() {
     totalDurationEl.textContent = formatTime(audio.duration);
-    // Automatically set the track name based on the file name
-    const filePath = audio.getAttribute('src');
-    const fileName = filePath.substring(filePath.lastIndexOf('/') + 1); // Extract file name
-    document.getElementById("trackname").querySelector('p').textContent = fileName;
-    trackname=fileName;
+    gettrackname();
     let currentindex = tracks.indexOf(trackname);
     if (currentindex==0||currentindex==3||currentindex==6||currentindex==9)
     {
@@ -184,7 +222,7 @@ let playerDiv=document.getElementById("player");
 };
 nextbtn.onclick=playnexttrack;
 previousbtn.onclick=function(){
-    let currentindex = tracks.indexOf(trackname);
+    let currentindex = tracks.indexOf(audio.src);
     if (currentindex==0||currentindex==3||currentindex==6||currentindex==9)
     {
         return;
@@ -196,7 +234,7 @@ previousbtn.onclick=function(){
 }
 previousbtn.onmouseenter= function ()
 {
-    let currentindex = tracks.indexOf(trackname);
+    let currentindex = tracks.indexOf(audio.src);
     if (currentindex==0||currentindex==3||currentindex==6||currentindex==9)
         {
             previousbtn.style.cursor='not-allowed';
